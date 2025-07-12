bc7fb30580e9e928fb6ca29d62a1e0aa717fdaa1
Himanshu <dhimanhimanshu9520@gmail.com>
2025-07-12 03:21:25 -0700

    add new file

---
+++ conditional.java
@@ -0,0 +1,11 @@
+public class conditional {
+    public static void main (String[] args){
+        int age=45;
+         if(age>18){
+            System.out.println("adult");
+         }else{
+            System.out.println("not adult");
+         }
+    }
+    
+}
